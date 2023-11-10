f3d03d63e03869cb102d3f29e7caa92f444c84d2
Holly Rivers <holly.isla.rivers@btinternet.com>
2023-11-10 08:24:17 +0000

    Implement printing a range of numbers

--- ReflectionFizzBuzz/FizzBuzz.cs
+++ ReflectionFizzBuzz/FizzBuzz.cs
@@ -1,6 +1,13 @@
 ﻿namespace ReflectionFizzBuzz; 
 
 public sealed class FizzBuzz {
+
+	public void PrintBetween(int minimum, int maximum) {
+		for (var i = minimum; i <= maximum; i++) {
+			Print(i);
+		}
+	}
+	
 	public void Print(int number) {
 		Console.WriteLine(number);
 	}
