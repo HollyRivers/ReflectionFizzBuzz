790da5fb3b2c009cd9a154a71a268f4acd529984
Holly Rivers <holly.isla.rivers@btinternet.com>
2023-11-10 08:16:28 +0000

    Test for printing number to console

--- ReflectionFizzBuzz.Tests/FizzBuzzTests.cs
+++ ReflectionFizzBuzz.Tests/FizzBuzzTests.cs
@@ -1,5 +1,18 @@
+using FluentAssertions;
+
 namespace ReflectionFizzBuzz.Tests;
 
 public class FizzBuzzTests {
-	
+	[Fact]
+	public void Print_WithValidNumber_PrintsNumberToConsole() {
+		var expectedOutput = $"1{Environment.NewLine}";
+		using var sw = new StringWriter();
+		Console.SetOut(sw);
+		var sut = new FizzBuzz();
+
+		sut.Print(1);
+
+		var actual = sw.ToString();
+		actual.Should().Be(expectedOutput);
+	}
 }
