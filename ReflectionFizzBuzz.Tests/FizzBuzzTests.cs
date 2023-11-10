1fb184f6374bdce1437129b3d06edcafb21ba9fd
Holly Rivers <holly.isla.rivers@btinternet.com>
2023-11-10 08:19:07 +0000

    Update test to theory that checks multiple different values

--- ReflectionFizzBuzz.Tests/FizzBuzzTests.cs
+++ ReflectionFizzBuzz.Tests/FizzBuzzTests.cs
@@ -3,14 +3,19 @@ using FluentAssertions;
 namespace ReflectionFizzBuzz.Tests;
 
 public class FizzBuzzTests {
-	[Fact]
-	public void Print_WithValidNumber_PrintsNumberToConsole() {
-		var expectedOutput = $"1{Environment.NewLine}";
+	[Theory]
+	[InlineData(1, "1")]
+	[InlineData(2, "2")]
+	[InlineData(3, "3")]
+	[InlineData(4, "4")]
+	[InlineData(5, "5")]
+	public void Print_WithValidNumber_PrintsNumberToConsole(int input, string expectedValue) {
+		var expectedOutput = $"{expectedValue}{Environment.NewLine}";
 		using var sw = new StringWriter();
 		Console.SetOut(sw);
 		var sut = new FizzBuzz();
 
-		sut.Print(1);
+		sut.Print(input);
 
 		var actual = sw.ToString();
 		actual.Should().Be(expectedOutput);
